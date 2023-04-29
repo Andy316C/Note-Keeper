686f5c45da5a4adffc792944231fbebe38520f66
andrew clark <andy-clark316@hotmail.com>
2023-04-29 15:18:37 +1000

    added server.js code and package.json

---
+++ Project/server.js
@@ -0,0 +1,23 @@
+// Import Express.js
+const express = require('express');
+// Import built-in Node.js package 'path' to resolve path of files that are located on the server
+const path = require('path');
+// Initialize an instance of Express.js
+const api= require("./routes");
+const app = express();
+const PORT = 3001;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', api);
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+)
+
+// listen() method is responsible for listening for incoming connections on the specified port 
+app.listen(PORT, () =>
+    console.log(`Example app listening at http://localhost:${PORT}`)
+);
